0a8c383fee36dae987ce65e83368dc35e9e426ca
declankra <declankramper@gmail.com>
2023-05-01 10:12:41 -0500

    moved folder

---
+++ Builds/Build 0.2.0/precache-manifest.b01c90e1f52fe35e04491c8ccbd5883a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c6a301618ebd7a3929b2",
+    "url": "/static/css/main.472af990.chunk.css"
+  },
+  {
+    "revision": "c6a301618ebd7a3929b2",
+    "url": "/static/js/main.7325df8f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9d649561e73463eff5a0",
+    "url": "/static/css/2.d9031211.chunk.css"
+  },
+  {
+    "revision": "9d649561e73463eff5a0",
+    "url": "/static/js/2.422b93f0.chunk.js"
+  },
+  {
+    "revision": "7e882c53cf09bc43d5f420611106c019",
+    "url": "/index.html"
+  }
+];
